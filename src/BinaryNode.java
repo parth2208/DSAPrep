6cd8e0e41ee6024405a0ff414b281042e780f1b8
Kaushal Aryan <kaushalaryan@Razias-MacBook-Air.local>
2020-02-20 13:44:20 +0530

    Initial commit

---
+++ src/BinaryNode.java
@@ -0,0 +1,7 @@
+
+public class BinaryNode {
+
+    public int data;
+    public BinaryNode left;
+    public BinaryNode right;
+}
---
+++ src/BinaryTree.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BinaryTree {
+
+    BinaryNode createNewNode(int k){
+        BinaryNode node = new BinaryNode();
+        node.data = k;
+        node.left = null;
+        node.right = null;
+
+        return node;
+    }
+
+    
+
+
+}
